2560bf932230149a3e9fd322a30963523a81a66c
Naffi <fakenaffi@gmail.com>
2023-10-09 14:15:08 +0200

    Added more fields to PostCreateSerializer.

--- web_app/api/serializers.py
+++ web_app/api/serializers.py
@@ -62,10 +62,34 @@ class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
 class PostCreateSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
     post_author = serializers.StringRelatedField(read_only=True)
+    display_name = serializers.StringRelatedField(read_only=True)
+    post_author_avatar = serializers.SerializerMethodField(read_only=True)
+    number_of_favorites = serializers.SerializerMethodField(read_only=True)
+    favorites = serializers.SerializerMethodField(read_only=True)
+    likes = serializers.SerializerMethodField(read_only=True)
+    is_liked = serializers.BooleanField(read_only=True)
+    is_favorite = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Post
-        exclude = ('number_of_comments', 'favorites')
+        fields = ['id', 'tags', 'post_author', 'post_author_avatar', 'display_name', 'content', 'created', 'image',
+                  'number_of_favorites', 'favorites', 'likes', 'is_liked', 'is_favorite', 'number_of_comments']
+
+    def get_post_author_avatar(self, instance):
+        return instance.post_author.avatar.url
+
+    def get_likes(self, instance):
+        return instance.get_total_like()
+
+    def get_favorites(self, instance):
+        favorite_users = instance.favorites.all()
+        return [user.username for user in favorite_users]
+
+    def get_number_of_favorites(self, instance):
+        return instance.get_total_favorites()
+
+    def get_display_name(self, instance):
+        return instance.post_author.display_name
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
